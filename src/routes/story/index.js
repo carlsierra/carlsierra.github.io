895ade71b99eb236c7f91d583bd790c4ed233ac2
Carl Johnson <carl.johnson@docusign.com>
2024-08-26 07:16:21 -0600

    our story

--- src/routes/story/index.js
+++ src/routes/story/index.js
@@ -1,9 +1,59 @@
-// import style from '../../style/index.css';
+import './style.css';
 
 export default function Story() {
 	return (
 		<div class='page'>
-			story coming soon
+			<h2>our story</h2>
+			<section>
+				Carl + Sierra both come from Michigan, although on opposite sides of the state; 
+				Carl grew up in the Detroit suburbs, while Sierra was raised in the beach town of Grand Haven. 
+				<br/><br/>
+				When Carl moved to Chicago after college, their paths decided to cross for the first time. 
+				Sierra had still been living in Michigan at the time, but would occasionally come into town 
+				to visit friends. Their Michigan social circles led them to meet! While their connection 
+				was friendly, they lost touch shortly afterward.
+				<img class="web-image" src='assets/telluride.JPG'/>
+			</section>
+			<section class="even">
+				<img class="web-image" src='assets/buena-vista.jpg'/>
+				After undergrad, Sierra moved to Denver for graduate school. 
+				She has always loved to ski and with her brother also living in Denver, she quickly felt right at home. 
+				That feeling led her to stay put while starting her career as an early childhood social worker.
+				<br/><br/>
+				A couple of years later, Carl’s software engineering job had gone remote. 
+				Being able to work from anywhere, he felt an itch to try something new. 
+				He called up a college buddy to ask if he could crash at his place in Denver for a couple of weeks. 
+				Not long after, he started researching apartments.
+			</section>
+			<section>
+			Shortly after moving into his own place, Carl conveniently set his dating app filter to a one-mile radius, 
+			and luck would have it Sierra popped up. The surprise of seeing familiar faces excited the two so 
+			they planned a date to get drinks. While the margaritas were heavily watered down, the conversation was not.
+			<br/><br/>
+			Carl + Sierra spent the next couple of years doing everything with each other, and it became clear that 
+			they were meant to be. Soon enough, they started house hunting and landed in a nice townhome near downtown Denver.
+			<img class="web-image" src='assets/paris.jpg'/>
+			</section>
+			<section class="even">
+			<img class="web-image" src='assets/engagement.jpg'/>
+			In June of 2024, after living in their new home for about a half year, they were slowly making 
+			it their own. Sierra planned a date night of ordering pizza, drinking wine, and learning how to 
+			paint a bathroom - it sounded wonderful. 
+			<br/><br/>
+			Little did she know, Carl had also been planning something wonderful. 
+			Once they finished painting, Carl got on one knee to ask her to marry him, and she said yes! 
+			It was a perfect, intimate, and spontaneous proposal.
+			</section>
+			<section>
+			Today, you can find the engaged couple biking around Denver, planning a weekend to the mountains, enjoying a 
+			jazz club, or cooking delicious food at home. They are extremely happy to be getting married and 
+			hope their closest friends and family can join them!
+			<img class="web-image" src='assets/hiking.jpg'/>
+			</section>
+			<section class="end">
+			With love,<br/><br/>
+			Carl + Sierra
+			</section>
 		</div>
 	);
 }

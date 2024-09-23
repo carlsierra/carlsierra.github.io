7070ec7ba5e9f8338addbbe1e643d32535552569
Carl Johnson <carl.johnson@docusign.com>
2024-09-22 20:26:48 -0600

    + banner pic and schedule

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,37 +6,38 @@ export default function Home() {
 			<div class="container">
 
 				<div class='stub landscape-stub-1'>
-					<img src='assets/home/KASI9084.jpg'/>
+				<img src='assets/home/KASI8911.jpg'/>
 				</div>
 
 				<div class='stub schedule-stub'>
 					<div>
-						<h3>Weekend Schedule</h3>
+						<h3>our Weekend Schedule</h3>
 
 						<div>
-							<h4>Thursday, May 29th, 2025</h4>
-							<p>7:00pm - Welcome party @ the Wild denver</p>
-							<p>10:00pm - Welcome party end</p>
+							<h4>Thursday, May 29th, 2025 | the wild denver</h4>
+							<p>7:00pm - Welcome party</p>
+							<p>10:00pm - Welcome party ends</p>
 						</div>
 						<div>
-							<h4>Friday, May 30th, 2025</h4>
-							<i>all events taking place @ moss denver</i>
+							<h4>Friday, May 30th, 2025 | moss denver</h4>
 							<p>5:30pm - Ceremony</p>
 							<p>6:00pm - cocktail hour</p>
 							<p>7:00pm - Dinner + Reception</p>
-							<p>11:00pm - Wedding end</p>
+							<p>11:00pm - celebration ends</p>
 						</div>
 					</div>
 
 				</div>
 				<div class='stub landscape-stub-2'>
-					<img src='assets/home/KASI9459.jpg'/>
+					
+					<img src='assets/home/KASI9084.jpg'/>
 				</div>
 				<div class='stub landscape-stub-3'>
 					<img src='assets/home/KASI7794.jpg'/>
 				</div>
 				<div class='stub landscape-stub-4'>
-					<img src='assets/home/KASI8911.jpg'/>
+					
+					<img src='assets/home/KASI9459.jpg'/>
 				</div>
 				<div class='stub landscape-stub-5'>
 					<img src='assets/home/KASI7892.jpg'/>
